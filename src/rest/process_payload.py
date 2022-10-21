96bd9cfc5b3b1a3a08eb3e0008a3d8be97b7b478
jjackman <jack.jackman@waltoninstitute.ie>
2022-10-21 09:28:40 +0100

    Adding validate functions to storage manager.

--- src/rest/process_payload.py
+++ src/rest/process_payload.py
@@ -1,5 +1,12 @@
+from utils import validate
+
 def store_data(data):
+    valid_pload = validate.validate_data(data)
+
     summary = {}
-    summary['body'] = "POST successful"
+    # report = store_data(valid_pload)
+    # summary.append(report)
+    
+    summary['body'] = "POST successful."
 
     return summary
