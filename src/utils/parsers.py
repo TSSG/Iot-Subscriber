fd0915887f79383ccff2a7b83a8adc4322998b9f
jjackman <jack.jackman@waltoninstitute.ie>
2022-10-21 09:52:24 +0100

    Fixing temporary transform_data function.

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -1,2 +1,2 @@
-def transform_data():
+def transform_data(data):
     return {'fields': {'time': '2022-10-18T11:12:00Z', 'value': 0.0}, 'time': '2022-10-18 11:12:00+00:00', 'measurement': 'location_36'}
