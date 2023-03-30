94d43f64e54e299ff4d3ddc4f7eac45771de5d87
Jack Jackman <jack.jackman@waltoninstitute.ie>
2023-03-30 09:45:43 +0100

    Changing construct_data_item to more readaable format.

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -5,43 +5,46 @@ taggable_fields = ["scenario", "mode", "id", "asset", "asset_id"]
 
 
 def construct_data_item(pload, topic):
+    """Converts received payload to a format that can be inserted to InfluxDB, and populates fields and tags."""
     data_item={}
     data_item["fields"] = {}
     tags = topic.copy()
-    for k in pload.keys():
-        if k in temporal_fields:
-            resolved_time = validate.resolve_time(pload[k])
+    # Iterate through each key in the payload
+    for key in pload.keys():
+        # Validate the timestamp if one is found
+        if key in temporal_fields:
+            resolved_time = validate.resolve_time(pload[key])
             data_item["time"] = resolved_time
-        if isinstance(pload[k], list):
-            if k not in to_be_ignored:
-                ndx = 1
-                for i in pload[k]:
-                    if isinstance(i, list):
-                        dx = 1
-                        for p in i:
-                            data_item["fields"][k+"_"+str(ndx)+"_"+str(dx)] = p
-                            dx+=1
-                        ndx+=1
+        # Add payload list to data_item to be inserted
+        if isinstance(pload[key], list):
+            if key not in to_be_ignored:
+                index_x = 1
+                # Flatten list to individual elements
+                for x in pload[key]:
+                    if isinstance(x, list):
+                        index_y = 1
+                        # Flatten nested list
+                        for y in x:
+                            data_item["fields"][key+"_"+str(index_x)+"_"+str(index_y)] = y
+                            index_y+=1
+                        index_x+=1
                     else:
-                        data_item["fields"][k+"_"+str(ndx)] = i
-                        ndx+=1
-        elif isinstance(pload[k], (float, int)):
-            if k not in to_be_ignored:
-                if k in taggable_fields:
-                    tags[k] = pload[k]
+                        data_item["fields"][key+"_"+str(index_x)] = x
+                        index_x+=1
+        # Add payload value or string to data_item to be inserted
+        elif isinstance(pload[key], (float, int, str)):
+            if key not in to_be_ignored:
+                # Determine if data is inserted as field or tag
+                if key in taggable_fields:
+                    tags[key] = pload[key]
                 else:
-                    data_item["fields"][k] = pload[k]
-        elif isinstance(pload[k], dict):
-            if k not in to_be_ignored:
-                data_item["fields"].update(pload[k])
-        elif isinstance(pload[k], str):
-            if k not in to_be_ignored:
-                if k in taggable_fields:
-                    tags[k] = pload[k]
-                else:
-                    data_item["fields"][k] = pload[k]
+                    data_item["fields"][key] = pload[key]
+        # Add payload dict to data_item to be inserted
+        elif isinstance(pload[key], dict):
+            if key not in to_be_ignored:
+                data_item["fields"].update(pload[key])
         else:
-            print(k+"not sure what k is but it has a value of \n "+ str(pload[k]))
+            print("(Transform): Could not process key " + key + " with value "+ str(pload[key]))
     data_item["tags"] = tags
     # Check if location is empty, if so, set to measurement
     if topic["location"] == '':
@@ -52,6 +55,7 @@ def construct_data_item(pload, topic):
 
 
 def split_topic(topic):
+    """Deconstructs MQTT topic to route data inserted to Influx"""
     tp_delim = {}
     tp = topic.split("/")
     ndx = 0
