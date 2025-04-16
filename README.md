3672bc408b6b7970a71d04aa32d47b04ab1e880a
Jack Jackman <jack.jackman@waltoninstitute.ie>
2025-04-16 11:34:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# IoT Subscriber
+
+This is a utility that captures data from an MQTT broker and routes it to an InfluxDB 2.X database. The routing information provided by the topic under which the data is published in the following format: `bucket/measurement/tag1/tag2/tag3...`. The Subscriber can insert data that is already structured to be written to InfluxDB, or unstructured data where any fields with a name matching one of the tags passed in through the topic are written to the database as tags.
+
+### Sample structured data:
+```json
+{
+	"time": 1682060040000,
+	"fields": {
+		"value": 50
+	},
+	"tags": {
+		"location": "waterford"
+	}
+}
+```
+
+### Sample unstructured data:
+```json
+{
+	"time": 1682050040000,
+	"value": 100,
+	"val_list": [60, 70, 80],
+	"location": "waterford"
+}
+```
+
+## Preconditions
+* GNU Make
+* docker & docker-compose
+* Existing InfluxDB containers should be added to the `subscriber-network` docker network. This has been done in the optional deployment below.
+* A local or remote MQTT broker such as [Eclipse Mosquitto](https://mosquitto.org/).
+* The conf.env file in the base directory should be populated with credentials for connecting to the MQTT broker and to the InfluxDB container.
+
+## Database deployment (optional)
+In the case where there is no pre-existing InfluxDB container, a method to deploy one has been provided. 
+
+To preconfigure the InfluxDB instance with username, password, retention policy, default bucket, etc, uncomment the first line in `db/conf.env` and fill out the relevant fields. 
+
+The URL for the container should be added to the conf.env at the base directory, eg, `DB_URL=http://iot-subscriber-persistance:8086`. 
+
+The data will persist in `db/storage/` and the configuration in `db/config/` if the InfluxDB container should exit.
+
+To deploy the InfluxDB container, `make build_db` from the base directory.
+
+## Deployment
+To deploy the Subscriber, navigate to the base directory in the terminal and 
+
+`make run` to run the `IoT Subscriber` service.
+
+`make cleanup` to cleanup.
+
+## License
+
+Maintained by [Jack Jackman](mailto:jack.jackman@waltoninstitute.ie) at the Walton Institute under the South East Technological University.
+
+This work was funded under the European Union Horizon 2020 research and innovation programme under grant agreement No 883710, project edgeFLEX (Providing flexibility to the grid by enabling VPPs to offer both fast and slow dynamics control services).
+
+This utility is released under the [Apache License, version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/mqtt_cli.py
@@ -0,0 +1,91 @@
+import paho.mqtt.client as paho
+import time
+
+from exceptions import invalid_host
+
+
+class MqttClient(object):
+    def __init__(self, **kwargs):
+
+        client_nm = "subscriber-base"
+        if "name" in kwargs:
+            client_nm = kwargs["name"]
+        self.mq_client = paho.Client(paho.CallbackAPIVersion.VERSION1, client_nm, True)
+        self.connected = False
+
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self.connected = True
+            print("Connected to MQTT Message Broker")
+            topic = "#"
+            self.mq_client.subscribe(topic)
+            msg = "Subscribed to: " + topic
+            print(msg)
+        else:
+            print("Failed to connect to MQTT Message Broker")
+
+
+    def on_disconnect(self, client, userdata, rc):
+        if rc == 0:
+            self.connected = False
+            print("Disconnected from MQTT Message Broker")
+        else:
+            print("Unexpected disconnect... Response code: " + str(rc))
+
+
+    def disconnect(self):
+        try:
+            self.mq_client.disconnect()
+            self.mq_client.loop_stop()
+        except Exception as error:
+            print("Disconnect error")
+            print(str(error))
+        return
+
+
+    def subscribe(self, topic):
+        return self.mq_client.subscribe(topic, 2)
+
+
+    def unsubscribe(self, topic):
+        return self.mq_client.unsubscribe(topic)
+
+
+    def get_client(self):
+        return self.mq_client
+
+
+    def is_connected(self):
+        return self.connected
+
+
+    def publish(self, topic, payload):
+        # Publish data to mqtt broker
+        self.mq_client.publish(topic, payload)
+
+
+    def connect(self, url, on_message, **kwargs):
+        if url == "" or url is None:
+            raise invalid_host.InvalidHost("%s is not a valid url for connection" %url)
+        try:
+            self.mq_client.on_connect = self.on_connect
+            self.mq_client.on_disconnect = self.on_disconnect
+            self.mq_client.on_message = on_message
+            mqtt_port = 1883
+            if "port" in kwargs:
+                mqtt_port = kwargs["port"]
+            if mqtt_port == 8883:
+                self.mq_client.username_pw_set(kwargs["user"], kwargs["passwd"])
+                self.mq_client.connect(
+                    url,
+                    port = mqtt_port,
+                    keepalive = 5)
+            else:
+                self.mq_client.connect(url, port = mqtt_port, keepalive=5)
+            self.mq_client.loop_start()
+            while not self.connected:
+                time.sleep(0.1)
+        except Exception as error:
+            print("Connect Error:", error)
+            raise 
